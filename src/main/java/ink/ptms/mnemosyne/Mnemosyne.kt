9f205567b02cb63cb113a0316fe2e8a4876db195
sky <sky@asgard.me>
2020-03-17 22:26:12 +0800

    Update 1.0

---
+++ src/main/java/ink/ptms/mnemosyne/Mnemosyne.kt
@@ -0,0 +1,10 @@
+package ink.ptms.mnemosyne
+
+import io.izzel.taboolib.loader.Plugin
+import io.izzel.taboolib.loader.PluginRedefine
+import io.izzel.taboolib.module.config.TConfig
+import io.izzel.taboolib.module.inject.TInject
+import io.izzel.taboolib.module.locale.logger.TLogger
+
+@Plugin.Version(5.20)
+class Mnemosyne : PluginRedefine()
